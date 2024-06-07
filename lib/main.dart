3e25d7e1d5b5525027a83ce272fa4fb41c379e4e
G10 <alpstone129152@gmail.com>
2024-06-08 01:31:50 +0530

    before_riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
